ea41aa465dfb2fe48a89d7c048eb1edda060b667
Leonid Suprun <mr.slay@gmail.com>
2014-02-25 13:58:29 +0400

    Subscribe to "onClicked" after Chrome restart

--- src/background.js
+++ src/background.js
@@ -5,26 +5,27 @@ chrome.runtime.onInstalled.addListener(function() {
 		"contexts":		['page'],
 		"id":			'WazeEditorReportSend'});
 
-	chrome.contextMenus.onClicked.addListener(function(info, tab) {
-		if (info.menuItemId != "WazeEditorReportSend") { return };
+});
 
-		var desc = prompt(chrome.i18n.getMessage('enterDescription'));
-		if (!desc) return;
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+	if (info.menuItemId != "WazeEditorReportSend") { return };
 
-		chrome.cookies.get({url: "https://www.waze.com", name: "_csrf_token"},
-			function(cookie) {
-				chrome.tabs.sendMessage(tab.id, 'getCurrentLocation', function(response) {
-					console.log('Got coordinates: ' + JSON.stringify(response));
-					
-					var xhr = new XMLHttpRequest();
-					xhr.open("POST", "https://www.waze.com/row-Descartes-live/app/MapProblems/UpdateRequest", true);
-					xhr.setRequestHeader('X-CSRF-Token', cookie.value);
-					xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
-					xhr.send("geometry=POINT(" + response.lat + "+" + response.lon + ")&" +
-						 "description=" + desc + "&" +
-						 "type=7");
-				});
-			}
-		);
-	});
+	var desc = prompt(chrome.i18n.getMessage('enterDescription'));
+	if (!desc) return;
+
+	chrome.cookies.get({url: "https://www.waze.com", name: "_csrf_token"},
+		function(cookie) {
+			chrome.tabs.sendMessage(tab.id, 'getCurrentLocation', function(response) {
+				console.log('Got coordinates: ' + JSON.stringify(response));
+				
+				var xhr = new XMLHttpRequest();
+				xhr.open("POST", "https://www.waze.com/row-Descartes-live/app/MapProblems/UpdateRequest", true);
+				xhr.setRequestHeader('X-CSRF-Token', cookie.value);
+				xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+				xhr.send("geometry=POINT(" + response.lat + "+" + response.lon + ")&" +
+					 "description=" + desc + "&" +
+					 "type=7");
+			});
+		}
+	);
 });
