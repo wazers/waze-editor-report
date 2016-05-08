7748bc8dfe7a3e1cebcdfd2ddc16a967e974cf82
Leonid Suprun <mr.slay@gmail.com>
2016-05-08 10:26:37 +0300

    Formatting fix

--- src/js/report-a-problem-ext.js
+++ src/js/report-a-problem-ext.js
@@ -4,7 +4,7 @@
 	var body = document.getElementsByTagName('body')[0];
 	var s = document.createElement('div');
 	s.setAttribute('id', 'wme-api-base');
-    s.setAttribute('style', 'display:none')
+	s.setAttribute('style', 'display:none')
 	s.innerHTML = W.Config.api_base;
 	body.appendChild(s);
 }());
